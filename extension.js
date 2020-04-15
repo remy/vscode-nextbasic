857b10d4a0a4aca95af410d4e02364f0be6ac3a2
Remy Sharp <remy@remysharp.com>
2020-04-15 23:20:44 +0100

    wip: adding formatter

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const { EOL } = require("os");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,8 @@ const vscode = require("vscode");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  const f = require("txt2bas");
+  console.log(Object.keys(f), f.formatText);
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
@@ -23,30 +26,53 @@ function activate(context) {
           const selection = editor.selection;
           const position = selection.end;
 
-          let currentLine = document
-            .getText(
-              document.getWordRangeAtPosition(
-                new vscode.Position(position.line, 0)
-              )
-            )
-            .split(" ")[0];
-          let nextLine = document
-            .getText(
-              document.getWordRangeAtPosition(
-                new vscode.Position(position.line + 1, 0)
-              )
-            )
-            .split(" ")[0];
+          const currentPosition = document.getWordRangeAtPosition(
+            new vscode.Position(position.line, 0)
+          );
+          let currentLine = currentPosition
+            ? document.getText(currentPosition).split(" ")[0]
+            : null;
+
+          // if we can't find anything, just drop in a new line, we're nice like that.
+          if (currentLine === null) {
+            editor.insertSnippet(
+              new vscode.SnippetString(`$0\n`),
+              new vscode.Position(position.line + 1, 0)
+            );
+            return;
+          }
+
+          const nextLinePos = document.getWordRangeAtPosition(
+            new vscode.Position(position.line + 1, 0)
+          );
+
+          let nextLine = nextLinePos
+            ? document.getText(nextLinePos).split(" ")[0]
+            : "9999";
 
           if (currentLine && nextLine) {
             currentLine = parseInt(currentLine, 10);
+
+            if (isNaN(currentLine)) {
+              return;
+            }
+
             nextLine = parseInt(nextLine, 10);
+
+            if (isNaN(nextLine)) {
+              nextLine = 9999;
+            }
+
             let d = currentLine + (((nextLine - currentLine) / 2) | 0);
             if (d !== nextLine && d !== currentLine) {
               if (d - currentLine > 10) d = currentLine + 10;
             }
+
+            const onLastLine =
+              position.line + 1 === document.getText().split("\n").length;
+
             editor.insertSnippet(
-              new vscode.SnippetString(`${d} $0\n`),
+              new vscode.SnippetString(`${onLastLine ? "\n" : ""}${d} $0\n`),
               new vscode.Position(position.line + 1, 0)
             );
           }
@@ -54,6 +80,34 @@ function activate(context) {
           console.log(e.message);
         }
       }
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.languages.registerOnTypeFormattingEditProvider(
+      ["nextbasic"],
+      {
+        provideOnTypeFormattingEdits(document, position, ch) {
+          try {
+            const line = document.lineAt(position.line - 1).text;
+            const formatted = formatText(line);
+            const editor = vscode.window.activeTextEditor;
+
+            console.log(line);
+            console.log(formatted);
+
+            editor.edit((editBuilder) => {
+              editBuilder.replace(
+                new vscode.Location(position.line - 1, position.character),
+                formatted
+              );
+            });
+          } catch (e) {
+            console.log(e);
+          }
+        },
+      },
+      EOL
     )
   );
 
