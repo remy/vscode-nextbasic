e5aa0b79374636952aa69c0d7b09fb1f44a68586
Remy Sharp <remy@remysharp.com>
2020-04-16 17:40:38 +0100

    format on enter, help, import + comments

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Change Log
+
+All notable changes to the "nextbasic" extension will be documented in this file.
+
+## [0.0.3] - 2020-04-16
+
+- On new line, the syntax is correctly capitalised (parsed into binary .bas and out to text)
+- Toggle single and multi-line comments (using VS Code's standard toggle comment shortcut), plus configuration to change between
+- New command "Convert .bas to text" to convert .bas binary to plain text
+- Hover over keywords for help definition and syntax
+
+## [0.0.2] - 2020-04-15
+
+- Fix line number insertion at end of doc or empty lines
+
+## [0.0.1] - 2020-04-15
+
+- Initial release
--- extension.js
+++ extension.js
@@ -1,7 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require("vscode");
-const { EOL } = require("os");
+const vscode = require('vscode');
+const newLineCommand = require('./lib/new-line.command');
+const importBasicCommand = require('./lib/import-basic.command');
+const toggleCommentsCommand = require('./lib/toggle-comments.command');
+const formatOnType = require('./lib/format-on-type.provider');
+const definitions = require('./lib/definition.provider');
+const hoverHelp = require('./lib/help.hover');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,155 +15,15 @@ const { EOL } = require("os");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  const f = require("txt2bas");
-  console.log(Object.keys(f), f.formatText);
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      "nextbasic.new-line-with-number",
-      function () {
-        try {
-          // The code you place here will be executed every time your command is executed
-          const editor = vscode.window.activeTextEditor;
-          const document = editor.document;
-          const selection = editor.selection;
-          const position = selection.end;
-
-          const currentPosition = document.getWordRangeAtPosition(
-            new vscode.Position(position.line, 0)
-          );
-          let currentLine = currentPosition
-            ? document.getText(currentPosition).split(" ")[0]
-            : null;
-
-          // if we can't find anything, just drop in a new line, we're nice like that.
-          if (currentLine === null) {
-            editor.insertSnippet(
-              new vscode.SnippetString(`$0\n`),
-              new vscode.Position(position.line + 1, 0)
-            );
-            return;
-          }
-
-          const nextLinePos = document.getWordRangeAtPosition(
-            new vscode.Position(position.line + 1, 0)
-          );
-
-          let nextLine = nextLinePos
-            ? document.getText(nextLinePos).split(" ")[0]
-            : "9999";
-
-          if (currentLine && nextLine) {
-            currentLine = parseInt(currentLine, 10);
-
-            if (isNaN(currentLine)) {
-              return;
-            }
-
-            nextLine = parseInt(nextLine, 10);
-
-            if (isNaN(nextLine)) {
-              nextLine = 9999;
-            }
-
-            let d = currentLine + (((nextLine - currentLine) / 2) | 0);
-            if (d !== nextLine && d !== currentLine) {
-              if (d - currentLine > 10) d = currentLine + 10;
-            }
-
-            const onLastLine =
-              position.line + 1 === document.getText().split("\n").length;
-
-            editor.insertSnippet(
-              new vscode.SnippetString(`${onLastLine ? "\n" : ""}${d} $0\n`),
-              new vscode.Position(position.line + 1, 0)
-            );
-          }
-        } catch (e) {
-          console.log(e.message);
-        }
-      }
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.languages.registerOnTypeFormattingEditProvider(
-      ["nextbasic"],
-      {
-        provideOnTypeFormattingEdits(document, position, ch) {
-          try {
-            const line = document.lineAt(position.line - 1).text;
-            const formatted = formatText(line);
-            const editor = vscode.window.activeTextEditor;
-
-            console.log(line);
-            console.log(formatted);
-
-            editor.edit((editBuilder) => {
-              editBuilder.replace(
-                new vscode.Location(position.line - 1, position.character),
-                formatted
-              );
-            });
-          } catch (e) {
-            console.log(e);
-          }
-        },
-      },
-      EOL
-    )
-  );
-
-  context.subscriptions.push(
-    vscode.languages.registerDefinitionProvider(["nextbasic"], {
-      provideDefinition(document, position) {
-        try {
-          const token = document.getWordRangeAtPosition(position);
-
-          const line = document
-            .lineAt(position)
-            .text.substring(0, token.start.character)
-            .trim()
-            .toLowerCase();
-
-          const jumpTerms = [
-            "goto",
-            "go to",
-            "gosub",
-            "go sub",
-            "proc",
-            "then",
-            "line",
-            "proc",
-          ];
-
-          if (jumpTerms.find((_) => line.endsWith(_))) {
-            const lines = document.getText().split("\n");
-            const lineNumber = document.getText(token);
-            let line = null;
-            if (/^\d+$/.test(lineNumber)) {
-              line = lines.findIndex((line) =>
-                line.startsWith(lineNumber + " ")
-              );
-            } else {
-              line = lines.findIndex((line) =>
-                line.includes(`DEFPROC ${lineNumber}(`)
-              );
-            }
-
-            if (line != null && line > -1) {
-              const pos = new vscode.Position(line, 0);
-              return new vscode.Location(document.uri, pos);
-            }
-          }
-        } catch (e) {
-          console.log(e.message);
-        }
-      },
-    })
-  );
+  context.subscriptions.push(newLineCommand());
+  context.subscriptions.push(importBasicCommand());
+  context.subscriptions.push(toggleCommentsCommand());
+  context.subscriptions.push(formatOnType());
+  context.subscriptions.push(definitions());
+  context.subscriptions.push(hoverHelp());
 }
 exports.activate = activate;
 
